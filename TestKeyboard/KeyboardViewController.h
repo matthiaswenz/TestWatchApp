a26114f1a5345288b6075018d5f9185ab21627c1
Matthias Wenz <matwen@microsoft.com>
2018-08-20 15:04:09 -0700

    Go

---
+++ TestKeyboard/KeyboardViewController.h
@@ -0,0 +1,13 @@
+//
+//  KeyboardViewController.h
+//  TestKeyboard
+//
+//  Created by Matthias Wenz on 8/20/18.
+//  Copyright © 2018 Matthias Wenz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KeyboardViewController : UIInputViewController
+
+@end
