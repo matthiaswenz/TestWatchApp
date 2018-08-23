123260a7b8bc1ca238ed2b963591e6b498a04477
Matthias Wenz <matwen@microsoft.com>
2018-08-23 16:13:23 -0700

    Add specific today extension

---
+++ TestToday/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  TestToday
+//
+//  Created by Matthias Wenz on 8/23/18.
+//  Copyright © 2018 Matthias Wenz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
